5329ebd67b9c206a73121d778cd8a8112a41bf07
sora <soraenjoyer@mail.ru>
2023-01-08 00:08:07 +0500

    hello world in raylib (based on cmake project template)

--- src/sfarx.c
+++ src/sfarx.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main(void)
+int screenWidth = 800;
+int screenHeight = 450;
+
+void UpdateDrawFrame(void);
+
+int main()
 {
-    puts("hello world");
+    InitWindow(screenWidth, screenHeight, "sfarx");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        UpdateDrawFrame();
+    }
+
+    CloseWindow();
     return 0;
+}
+
+void UpdateDrawFrame(void)
+{
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+    DrawText("hello world!", 190, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
 }
